a46d10a5019773ea0018056ce618ea7ef0c2d7d1
Ilya <ilya-kubanov@yandex.ru>
2018-10-25 20:46:18 +0300

    25.10

---
+++ database/migrations/2018_10_25_170308_create_static_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaticPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('static_pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url')->unique();
+            $table->string('title');
+            $table->string('description');
+            $table->string('heading');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('static_pages');
+    }
+}
---
+++ resources/views/static.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="static-content">
+
+
+
+    </div>
+
+@endsection
